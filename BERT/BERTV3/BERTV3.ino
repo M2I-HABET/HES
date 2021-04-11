5deca2643c6082020e5298d0fd1494e0375dbc5f
Matt Kreul <mjkreul@iastate.edu>
2021-04-10 19:32:21 -0500

    BERTV3 is working for now albeit without printing to a file...

--- BERT/BERTV3/BERTV3.ino
+++ BERT/BERTV3/BERTV3.ino
@@ -19,7 +19,6 @@
   Date: August 22nd, 2018
   License: MIT. See license file for more information but you can
   basically do whatever you want with this code.
-
   This example reads the NMEA characters over I2C and pipes them to MicroNMEA
   This example will output your current long/lat and satellites in view
  
@@ -28,9 +27,7 @@
   ZED-F9P RTK2: https://www.sparkfun.com/products/15136
   NEO-M8P RTK: https://www.sparkfun.com/products/15005
   SAM-M8Q: https://www.sparkfun.com/products/15106
-
   For more MicroNMEA info see https://github.com/stevemarple/MicroNMEA
-
   Hardware Connections:
   Plug a Qwiic cable into the GNSS and a BlackBoard
   If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
@@ -39,67 +36,71 @@
 */
 
 #include <Wire.h> //Needed for I2C to GNSS
-#include "Zanshin_BME680.h"  // Include the BME680 Sensor library
-
-/**************************************************************************************************
-** Declare global variables and instantiate classes                                              **
-**************************************************************************************************/
-BME680_Class BME680;  ///< Create an instance of the BME680 class
-///< Forward function declaration with default value for sea level
-float altitude(const int32_t press, const float seaLevel = 1013.25);
-float altitude(const int32_t press, const float seaLevel) {
-  /*!
-  @brief     This converts a pressure measurement into a height in meters
-  @details   The corrected sea-level pressure can be passed into the function if it is known,
-             otherwise the standard atmospheric pressure of 1013.25hPa is used (see
-             https://en.wikipedia.org/wiki/Atmospheric_pressure) for details.
-  @param[in] press    Pressure reading from BME680
-  @param[in] seaLevel Sea-Level pressure in millibars
-  @return    floating point altitude in meters.
-  */
-  static float Altitude;
-  Altitude =
-      44330.0 * (1.0 - pow(((float)press / 100.0) / seaLevel, 0.1903));  // Convert into meters
-  return (Altitude);
-}  // of method altitude()
-
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
-SFE_UBLOX_GNSS myGNSS;
-
 #include <MicroNMEA.h> //http://librarymanager/All#MicroNMEA
+#include <Arduino.h>
+#include "Adafruit_SHT31.h"
+#include <Adafruit_BMP280.h>
+
+SFE_UBLOX_GNSS myGNSS;
 char nmeaBuffer[100];
 MicroNMEA nmea(nmeaBuffer, sizeof(nmeaBuffer));
+
+bool enableHeater = false;
+uint8_t loopCnt = 0;
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+
+
+Adafruit_BMP280 bmp; // use I2C interface
+Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
 
 void setup()
 {
   Serial.begin(115200);
+  delay(1000);
   Serial.println("SparkFun u-blox Example");
 
   Wire.begin();
-
-  Serial.print(F("Starting I2CDemo example program for BME680\n"));
-  Serial.print(F("- Initializing BME680 sensor\n"));
-  while (!BME680.begin(I2C_STANDARD_MODE)) {  // Start BME680 using I2C, use first device found
-    Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
-    delay(5000);
-  }  // of loop until device is located
-  Serial.print(F("- Setting 16x oversampling for all sensors\n"));
-  BME680.setOversampling(TemperatureSensor, Oversample16);  // Use enumerated type values
-  BME680.setOversampling(HumiditySensor, Oversample16);     // Use enumerated type values
-  BME680.setOversampling(PressureSensor, Oversample16);     // Use enumerated type values
-  Serial.print(F("- Setting IIR filter to a value of 4 samples\n"));
-  BME680.setIIRFilter(IIR4);  // Use enumerated type values
-  Serial.print(F("- Setting gas measurement to 320\xC2\xB0\x43 for 150ms\n"));  // "�C" symbols
-  BME680.setGas(320, 150);  // 320�c for 150 milliseconds
-  delay(750); //Wait for I2C to settle
 
   if (myGNSS.begin() == false)
   {
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
     while (1);
   }
+
+  Serial.println("SHT31 test");
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+
+  Serial.print("Heater Enabled State: ");
+  if (sht31.isHeaterEnabled())
+    Serial.println("ENABLED");
+  else
+    Serial.println("DISABLED");
+
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    while (1) delay(10);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  bmp_temp->printSensorDetails();
   
 }
+  
+
 
 void loop()
 {
@@ -155,43 +156,39 @@ void loop()
     else if (RTK == 2) Serial.print(F("2:"));
     Serial.println("END");
   }
-  /* ==================================
-   *           BME 680 Data
-   * ==================================
-   */
 
-   /*!
-  @brief    Arduino method for the main program loop
-  @details  This is the main program for the Arduino IDE, it is an infinite loop and keeps on
-            repeating. The "sprintf()" function is to pretty-print the values, since floating
-            point is not supported on the Arduino, split the values into those before and those
-            after the decimal point.
-  @return   void
-  */
+  
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
 
-  static int32_t  temp, humidity, pressure, gas;  // BME readings
-  static char     buf[16];                        // sprintf text buffer
-  static float    alt;                            // Temporary variable
-  static uint16_t loopCounter = 0;                // Display iterations
-                                                    // if-then time to show headers
-  BME680.getSensorData(temp, humidity, pressure, gas);  // Get readings
-  if (loopCounter++ != 0) {                             // Ignore first reading, might be incorrect
-    sprintf(buf, "%4d %3d.%02d:", (loopCounter - 1) % 9999,  // Clamp to 9999,
-            (int8_t)(temp / 100), (uint8_t)(temp % 100));   // Temp in decidegrees
-    Serial.print(buf);
-    sprintf(buf, "%3d.%03d:", (int8_t)(humidity / 1000),
-            (uint16_t)(humidity % 1000));  // Humidity milli-pct
-    Serial.print(buf);
-    sprintf(buf, "%7d.%02d:", (int16_t)(pressure / 100),
-            (uint8_t)(pressure % 100));  // Pressure Pascals
-    Serial.print(buf);
-    alt = altitude(pressure);                                                // temp altitude
-    sprintf(buf, "%5d.%02d:", (int16_t)(alt), ((uint8_t)(alt * 100) % 100));  // Altitude meters
-    Serial.print(buf);
-    sprintf(buf, "%4d.%02d\n", (int16_t)(gas / 100), (uint8_t)(gas % 100));  // Resistance milliohms
-    Serial.print(buf);
-    delay(1000);  // Wait 10s
-  }                // of ignore first reading
+  if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+  
+  if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else { 
+    Serial.println("Failed to read humidity");
+  }
+  
+  sensors_event_t temp_event, pressure_event;
+  bmp_temp->getEvent(&temp_event);
+  bmp_pressure->getEvent(&pressure_event);
+  
+  Serial.print(F("Temperature = "));
+  Serial.print(temp_event.temperature);
+  Serial.println(" *C");
+
+  Serial.print(F("Pressure = "));
+  Serial.print(pressure_event.pressure);
+  Serial.println(" hPa");
+
+  Serial.println();
+    
+  delay(10000);
+ 
 }
 
 //This function gets called from the SparkFun u-blox Arduino Library
